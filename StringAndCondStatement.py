7145dba590524257db5ec7567e7147be9b6b93c1
UTTAM KUMAR <rajuttam4439@gmail.com>
2026-02-06 13:52:32 +0530

    Strings + 1

--- StringAndCondStatement.py
+++ StringAndCondStatement.py
@@ -2,3 +2,18 @@ name = "Uttam Kumar"
 print(name[0])
 print(len(name))
 print(name[-5:-1])
+
+print(name.endswith("ar"))
+print(name.endswith("ma"))
+
+job = "advanced Associate Software Engineer"
+print(job.capitalize()) #This prints the string first letter in capital.
+print(job) 
+""" Even just changing to capital in previous line it still print in small because it job.capitalize() create new 
+string and make changes For making changes in the actual String you have assign the changes to the old ones like
+job = job.capitalize() """
+
+print(job.replace("Software", "AI")) # software word is replaced with AI
+print(job.find("AI")) # This gives -1 because it can't find anything like that
+
+print(job.count("nced")) # count the occurance of the substring
