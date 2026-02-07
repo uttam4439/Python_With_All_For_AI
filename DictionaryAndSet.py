3c098e7afc4ff627bcf4f8ddd4e3080c16d6f063
UTTAM KUMAR <rajuttam4439@gmail.com>
2026-02-07 23:05:31 +0530

    Dictionary & Set

---
+++ DictionaryAndSet.py
@@ -0,0 +1,10 @@
+Student = { "name" : "Uttam Kumar" ,
+           "Subject_Marks" : 
+           {"Phy" : 68 ,
+            "Chem" : 70 ,
+            "Maths" : 80}
+}
+
+print(Student)
+print(Student["Subject_Marks"])
+print(Student["Subject_Marks"]["Chem"])
